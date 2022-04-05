c8bc785df1d918f71fb7c3d3ccacec1609213c96
Jonas Carlsson <rydhem80@hotmail.com>
2022-04-05 12:36:38 +0200

    Commit Test

---
+++ TodoIT/Model/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIT.Model
+{
+    class ToDo
+    {
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        public ToDo(int todoIt, string description) { }
+    }
+
+    
+}
