a08e2341e147d51677bf3074dcba57e22c0f8b82
Benjamin Nordin <godiset@gmail.com>
2022-04-05 12:34:18 +0200

    Add Person.cs Constructor

--- TodoIT/Model/Person.cs
+++ TodoIT/Model/Person.cs
@@ -2,6 +2,30 @@
 {
     public class Person
     {
+        private readonly int personId;
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+
+        private readonly string defaultFirstName = "Steve";
+        private readonly string defaultLastName = "Jobs";
+
+        public Person(int personId, string firstName, string lastName)
+        {
+            this.personId = personId;
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+            if (string.IsNullOrEmpty(firstName))
+            {
+                this.firstName = this.defaultFirstName;
+            }
+
+            if (string.IsNullOrEmpty(lastName))
+            {
+                this.lastName = this.defaultLastName;
+            }
+        }
         
     }
 }
